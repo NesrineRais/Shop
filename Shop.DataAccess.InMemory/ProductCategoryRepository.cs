61de85b14c7ad75a5832a8f2f394b147803b24f9
rais.nesrine@gmail.com <rais.nesrine@gmail.com>
2021-01-19 12:26:48 +0100

    Creation Category

---
+++ Shop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,84 @@
+﻿using Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productsCategories;
+
+        public ProductCategoryRepository()
+        {
+            //bonne pratique
+            productsCategories = cache["ProductCategory"] as List<ProductCategory>; //as sert a faire un cast
+            if (productsCategories == null)
+            {
+                productsCategories = new List<ProductCategory>();
+            }
+
+
+        }
+        public void SaveChanges()
+        {
+            cache["productCategories"] = productsCategories;
+        }
+
+        public void Inser(ProductCategory p)
+        {
+            productsCategories.Add(p);
+        }
+        public void Update(ProductCategory p)
+        {
+            ProductCategory prodToUpdate = productsCategories.Find(prod => prod.Id == p.Id);
+            if (prodToUpdate != null)
+            {
+                prodToUpdate = p;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        public ProductCategory FindById(int id)
+        {
+            ProductCategory p = productsCategories.Find(prod => prod.Id == id);
+
+            if (p != null)
+            {
+                return p;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        //Le type Iquerable accéte les reqiuéte LINQ contrairement a une LIst classique
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productsCategories.AsQueryable();
+        }
+
+        public void Delete(int id)
+        {
+            ProductCategory ProdToDelete = productsCategories.Find(p => p.Id == id);
+            if (ProdToDelete != null)
+            {
+                productsCategories.Remove(ProdToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+
+    }
+}
+
---
+++ Shop.WebUi/Controllers/ProductCategoryController.cs
@@ -0,0 +1,154 @@
+﻿using Shop.Core.Models;
+using Shop.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.WebUi.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        ProductCategoryRepository context;
+
+        public ProductCategoryController()
+        {
+            context = new ProductCategoryRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+        }
+
+        public ActionResult Create()//on parle sur la page creation qui vas recevoir un formaulaire de création d un produit
+        {
+            ProductCategory p = new ProductCategory();
+            return View(p);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductCategory product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product); //RESTER SUR LA M2ME PAGE AVEC LES MESSAGE ERREUR QUI VA AFFICHER
+            }
+            else
+            {
+                context.Inser(product);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ProductCategory p = context.FindById(id);
+            try
+            {
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProductCategory product, int id)
+        {
+            try
+            {
+                ProductCategory prodToEdit = context.FindById(id);
+                if (prodToEdit == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return View(product);
+                    }
+                    else
+                    {
+                        //context.Update(product); ce n'est pas un context entity framework
+                        prodToEdit.Category = product.Category;
+                       
+                        context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+
+            }
+            catch
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                ProductCategory p = context.FindById(id);
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirDelete(int id)
+        {
+            try
+            {
+                ProductCategory prodToDelete = context.FindById(id);
+                if (prodToDelete == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    context.Delete(id);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+
+
+        }
+
+    }
+}
